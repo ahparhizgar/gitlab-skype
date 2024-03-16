88170543ab4275581c75069a681d99e3843328b3
Amir Hossein Parhizgar <am.parhizgar@gmail.com>
2024-03-16 15:22:56 +0330

    Implement the extension

--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,53 @@
-(() => {
-    let youtubeLeftControls, youtubePlayer;
-    let currentVideo = "";
-    let currentVideoBookmarks = [];
-
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
-
-        if (type === "NEW") {
-            currentVideo = videoId;
-            newVideoLoaded();
+function onPageLoaded() {
+    const copyButton = document.getElementsByClassName("copy-button")[0];
+    if (!copyButton) {
+        const buildButton = (icon, description, textToCopy) => {
+            const tempContainer = document.createElement("div")
+            tempContainer.innerHTML = `<button class="gl-button btn btn-icon btn-sm btn-default btn-default-tertiary 
+        gl-display-none! gl-md-display-inline-block! js-source-branch-copy gl-mx-1" 
+         aria-keyshortcuts="b"
+          aria-live="polite" data-toggle="tooltip" 
+         data-placement="bottom" data-container="body" data-html="true" 
+          type="button">
+         <img class="copy-button s16 gl-icon gl-button-icon " title="Click to bookmark current timestamp" alt="copy">
+        </button>`
+            const button = tempContainer.getElementsByTagName("button")[0]
+            button.setAttribute("data-clipboard-text", textToCopy)
+            button.setAttribute("title", description + ` <kbd class='flat ml-1' aria-hidden=true>b</kbd>`)
+            button.setAttribute("aria-label", description)
+            const copyButton = tempContainer.getElementsByClassName("copy-button")[0];
+            copyButton.src = chrome.runtime.getURL(icon);
+            return tempContainer.firstElementChild
         }
-    });
-
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
-
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
-
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
-
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
-            youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+        const mrIdElement = document.querySelector('[data-testid="breadcrumb-links"] > ul > li:last-of-type > a');
+        if (!mrIdElement) {
+            console.error("MR id element not found")
+            return
         }
+
+        const mrTitleElement =  document.querySelector('[data-testid="title-content"]');
+        if (!mrTitleElement) {
+            console.error("MR title element not found")
+            return
+        }
+
+        const copyReviewButton = buildButton(
+            "assets/source_notes.svg",
+            "Copy Review Request",
+            "Please review [" + mrTitleElement.innerHTML.trim() + "](" + mrIdElement.href + ")"
+        )
+        const copyCheckButton = buildButton(
+            "assets/mark_chat_read.svg",
+            "Copy Check Request",
+            "Please check [" + mrTitleElement.innerHTML.trim() + "](" + mrIdElement.href + ")"
+        )
+        const mergeDescription = document.getElementsByClassName("detail-page-description")[0];
+        mergeDescription.append(copyReviewButton);
+        mergeDescription.append(copyCheckButton);
+        const stickyMergeDescription = document.querySelector('.issue-sticky-header > div > div')
+        stickyMergeDescription.appendChild(copyReviewButton.cloneNode(true))
+        stickyMergeDescription.appendChild(copyCheckButton.cloneNode(true))
     }
-
-    const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
-
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
-
-    newVideoLoaded();
-})();
-
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
-
-    return date.toISOString().substr(11, 0);
 }
+
+window.addEventListener('load', onPageLoaded);
