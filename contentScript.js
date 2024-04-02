bb66ba0a5f397beca8009183093e97f99d2acdaa
Amir Hossein Parhizgar <am.parhizgar@gmail.com>
2024-04-02 15:24:22 +0330

    Fix load and shortcut conflict problem

--- contentScript.js
+++ contentScript.js
@@ -1,67 +1,92 @@
-function onPageLoaded() {
-    const copyButton = document.getElementsByClassName("copy-button")[0];
-    if (!copyButton) {
-        const buildButton = (icon, description, textToCopy, shortcut) => {
-            const tempContainer = document.createElement("div")
-            tempContainer.innerHTML = `<button class="my-shortcut gl-button btn btn-icon btn-sm btn-default btn-default-tertiary 
-        gl-display-none! gl-md-display-inline-block! js-copy-reference gl-mx-1" 
-         aria-keyshortcuts="${shortcut}"
-          aria-live="polite" data-toggle="tooltip" 
-         data-placement="bottom" data-container="body" data-html="true"  
-         data-track-action="click_button"
-          type="button">
-         <img class="copy-button s16 gl-icon gl-button-icon " title="Click to bookmark current timestamp" alt="copy">
+async function onPageLoaded() {
+    const buildButton = (icon, description, textToCopy, shortcut) => {
+        return `<button class="my-shortcut gl-button btn btn-icon btn-sm btn-default btn-default-tertiary gl-display-none! gl-md-display-inline-block! js-copy-reference gl-mx-1" aria-live="polite" data-toggle="tooltip" data-placement="bottom" data-container="body" data-html="true" data-track-action="click_button" type="button"
+        title="${description} <kbd class='flat ml-1' aria-hidden=true>${shortcut}</kbd>"
+        data-clipboard-text="${textToCopy}"
+        aria-keyshortcuts="${shortcut}"
+        aria-label="${description}">
+        <img class="copy-button s16 gl-icon gl-button-icon " title="Click to bookmark current timestamp" alt="copy" 
+        src="${chrome.runtime.getURL(icon)}">
         </button>`
-            const button = tempContainer.getElementsByTagName("button")[0]
-            button.setAttribute("data-clipboard-text", textToCopy)
-            button.setAttribute("title", description + ` <kbd class='flat ml-1' aria-hidden=true>${shortcut}</kbd>`)
-            button.setAttribute("aria-label", description)
-            const copyButton = tempContainer.getElementsByClassName("copy-button")[0];
-            copyButton.src = chrome.runtime.getURL(icon);
-            return tempContainer.firstElementChild
-        }
-        const mrIdElement = document.querySelector('[data-testid="breadcrumb-links"] > ul > li:last-of-type > a');
-        if (!mrIdElement) {
-            console.error("MR id element not found")
-            return
-        }
-
-        const mrTitleElement = document.querySelector('[data-testid="title-content"]');
-        if (!mrTitleElement) {
-            console.error("MR title element not found")
-            return
-        }
-
-        const copyReviewButton = buildButton(
-            "assets/source_notes.svg",
-            "Copy Review Request",
-            "Please review [" + mrTitleElement.innerHTML.trim() + "](" + mrIdElement.href + ")",
-            "r"
-        )
-        const copyCheckButton = buildButton(
-            "assets/mark_chat_read.svg",
-            "Copy Check Request",
-            "Please check [" + mrTitleElement.innerHTML.trim() + "](" + mrIdElement.href + ")",
-            "c"
-        )
-        const mergeDescription = document.getElementsByClassName("detail-page-description")[0];
-        mergeDescription.append(copyReviewButton);
-        mergeDescription.append(copyCheckButton);
-        const stickyMergeDescription = document.querySelector('.issue-sticky-header > div > div')
-        stickyMergeDescription.appendChild(copyReviewButton.cloneNode(true))
-        stickyMergeDescription.appendChild(copyCheckButton.cloneNode(true))
     }
+    const mrIdElement = await waitForElement('[data-testid="breadcrumb-links"] > ul > li:last-of-type > a');
+    const mrTitleElement = await waitForElement('[data-testid="title-content"]');
+
+    function addButtons(parent) {
+        parent.insertAdjacentHTML("afterend", buildButton(
+                "assets/source_notes.svg",
+                "Copy Review Request",
+                "Please review [" + mrTitleElement.innerHTML.trim() + "](" + mrIdElement.href + ")",
+                "r"
+            )
+        )
+        parent.insertAdjacentHTML("afterend", buildButton(
+                "assets/mark_chat_read.svg",
+                "Copy Check Request",
+                "Please check [" + mrTitleElement.innerHTML.trim() + "](" + mrIdElement.href + ")",
+                "c"
+            )
+        )
+    }
+
+    addButtons(await waitForElement(`.detail-page-description > :last-child`))
+    addButtons(await waitForElement('.issue-sticky-header > div > div > :last-child'));
 }
-onPageLoaded()
-window.addEventListener('load', function () {
-    onPageLoaded()
+
+function waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(() => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+window.addEventListener('load', async function () {
+    await onPageLoaded()
     const shortcutElements = document.querySelectorAll('.my-shortcut');
     shortcutElements.forEach(element => {
         const shortcuts = element.getAttribute('aria-keyshortcuts');
-        document.addEventListener('keydown', function(event) {
-            if (shortcuts && shortcuts.includes(event.key)) {
-                element.click();
+        document.addEventListener('keydown', function (event) {
+            if (!hasModifierKeys(event) && shortcuts.includes(event.key)) {
+                if (isElementInViewport(element) && !isInInvisibleParent(element))
+                    element.click();
             }
         });
     });
 });
+
+function hasModifierKeys(event) {
+    return event.ctrlKey || event.altKey || event.shiftKey;
+}
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.bottom > 0 &&
+        rect.right > 0 &&
+        rect.left < window.innerWidth &&
+        rect.top < window.innerHeight;
+}
+
+function isInInvisibleParent(element) {
+    let parent = element;
+    while (parent && parent !== document.body) {
+        const computedStyle = window.getComputedStyle(parent);
+        if (computedStyle.display === 'none' || computedStyle.visibility === 'hidden') {
+            return true;
+        }
+        parent = parent.parentNode;
+    }
+    return false;
+}
